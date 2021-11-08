b310fe5433d8eefd9f7094d1e15106520dfe4aa7
Nouu <noelarrieulacoste@yahoo.fr>
2021-11-08 19:39:00 +0100

    fill user model

--- src/main/java/org/larrieulacoste/noe/al/domain/entity/User.java
+++ src/main/java/org/larrieulacoste/noe/al/domain/entity/User.java
@@ -1,4 +1,54 @@
 package org.larrieulacoste.noe.al.domain.entity;
 
-public class User {
+import java.util.Objects;
+
+public final class User {
+
+    private final UserId userId;
+    private final String lastname;
+    private final String firstname;
+    private String email;
+    private String password;
+    private String bankAccount;
+
+    private User(UserId userId, String lastname, String firstname, String email, String password, String bankAccount) {
+        this.userId = Objects.requireNonNull(userId);
+        this.lastname = Objects.requireNonNull(lastname);
+        this.firstname = Objects.requireNonNull(firstname);
+        this.email = Objects.requireNonNull(email);
+        this.password = Objects.requireNonNull(password);
+        this.bankAccount = Objects.requireNonNull(bankAccount);
+    }
+
+    static User of(UserId userId, String lastname, String firstname, String email, String password, String bankAccount) {
+        return new User(userId, lastname, firstname, email, password, bankAccount);
+    }
+
+    static User of(String lastname, String firstname, String email, String password, String bankAccount) {
+        return new User(UserId.create(), lastname, firstname, email, password, bankAccount);
+    }
+
+    void changeEmail(String newEmail) {
+        this.email = Objects.requireNonNull(newEmail);
+    }
+
+    void changePassword(String newPassword) {
+        this.password = Objects.requireNonNull(newPassword);
+    }
+
+    void changeBankAccount(String newBankAccount) {
+        this.bankAccount = Objects.requireNonNull(newBankAccount);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", bankAccount='" + bankAccount + '\'' +
+                '}';
+    }
 }
